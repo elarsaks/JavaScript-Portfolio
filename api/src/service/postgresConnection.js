b09b43a752be528da532243a78b3e29c20aca9d9
elarsaks <elarsaks@gmail.com>
2021-01-08 03:30:24 +0200

    Create database connection.

--- api/src/service/postgresConnection.js
+++ api/src/service/postgresConnection.js
@@ -1,11 +1,11 @@
 import pgPromise from 'pg-promise'
 
 const postgresConfig = {
-  host: '127.0.0.1',
-  port: '5432',
-  database: 'solita',
-  user: 'postgres',
-  password: 'password',
+  host: process.env.POSTGRES_HOST || '127.0.0.1',
+  port: process.env.POSTGRES_PORT || 5432,
+  database: process.env.POSTGRES_DB || 'solita',
+  user: process.env.POSTGRES_USERNAME || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'password',
 }
 
 const pgp = pgPromise()
