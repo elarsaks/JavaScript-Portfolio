9c5774b31f858005eefa97f893036d2add1a557f
elarsaks <elarsaks@gmail.com>
2021-01-18 11:39:34 +0200

    Fix api link, when running in cloud.

--- client/src/repository/Names.js
+++ client/src/repository/Names.js
@@ -2,7 +2,9 @@ const axios = require('axios')
 
 function getNames(params) {
   return axios({
-    url: `http://localhost:3001/names`,
+    // Link for local testing
+    //url: `http://localhost:3001/names`
+    url: `http://35.228.165.194:3001/names`,
     method: 'GET',
     params: params,
   }).then(resp => resp.data)
