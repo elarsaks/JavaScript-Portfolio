f493b736d0fbdc616b12f5d8c83b8609ad6337a0
elarsaks <elarsaks@gmail.com>
2021-02-11 08:30:47 +0200

    Add app name to vue-client.

--- vue-client/src/components/AppBar.vue
+++ vue-client/src/components/AppBar.vue
@@ -1,40 +1,32 @@
 <template>
-     <div id="app-bar-wrapper">
-        <v-app-bar
-            color="primary"
-            dense
-            dark
-        >
-            <v-tabs centered>
-                <v-tab
-                    v-for="page in pages"
-                    :key="page"
-                    :to="page"
-                >
-                    {{page}}
-                </v-tab>
-            </v-tabs>
-            <v-spacer></v-spacer>
-        </v-app-bar>
-    </div>
+  <div id="app-bar-wrapper">
+    <v-app-bar color="primary" dense dark>
+      <v-app-bar-title>NAMES</v-app-bar-title>
+      <v-tabs centered>
+        <v-tab v-for="page in pages" :key="page" :to="page">
+          {{ page }}
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'AppBar',
-  props:{
-      pages: {
-          type: Array,
-          required: true,
-      }
-  }
-};
+  props: {
+    pages: {
+      type: Array,
+      required: true,
+    },
+  },
+}
 </script>
 
 <style scoped>
 #app-bar-wrapper {
-    position: fixed;
-    width: 100%;
+  position: fixed;
+  width: 100%;
 }
 </style>
