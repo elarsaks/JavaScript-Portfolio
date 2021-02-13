6a8ffb9bedc1be6fae1c20d8e5f4269f937de960
elarsaks <elarsaks@gmail.com>
2021-02-13 19:58:37 +0200

    Fix some typos.

--- vue-client/src/router/components/Info.vue
+++ vue-client/src/router/components/Info.vue
@@ -2,9 +2,10 @@
   <div id="info">
     <h1>INFO</h1>
     <h4>
-      This application is a built as part of my full-stack JavaScript portfolio.
+      This application is built as part of my full-stack JavaScript portfolio.
       <br />
-      You can find the exact same front end built in vue and Vuetify from
+      You can find the exact same front end built with React and Materialize
+      from
       <a href="http://35.228.165.194:3000" target="_blank"> here </a>.
     </h4>
     <br />
